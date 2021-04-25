7fda4f78cbf557b4f27f0941bff0323b53fce5f9
vigneshofficial2020 <vigneshofficial2020@gmail.com>
2021-04-25 17:24:35 +0530

    optional type can be used

--- tutorial.js
+++ tutorial.js
@@ -1,12 +1,4 @@
-var complex = {
-    age: [34, 34, 23, 123],
-    getAgeDetails: function (all) {
-        return this.age;
-    }
-};
-var complex2 = {
-    age: [34, 343434, 12312312321],
-    getAgeDetails: function (all) {
-        return this.age;
-    }
-};
+var ages = 56;
+ages = true;
+ages = 56;
+console.log(typeof ages);
--- tutorial.ts
+++ tutorial.ts
@@ -1,18 +1,4 @@
-type ComplexDetails = {
-  age: number[];
-  getAgeDetails: (all: boolean) => number[];
-};
-
-let complex: ComplexDetails = {
-  age: [34, 34, 23, 123],
-  getAgeDetails: function (all: boolean): number[] {
-    return this.age;
-  },
-};
-
-let complex2: ComplexDetails = {
-  age: [34, 343434, 12312312321],
-  getAgeDetails: function (all: boolean): number[] {
-    return this.age;
-  },
-};
+let ages: number | boolean = 56;
+ages = true;
+ages = 56;
+console.log(typeof ages);
