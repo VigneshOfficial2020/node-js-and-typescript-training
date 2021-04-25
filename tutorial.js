29018dfdb4432c2c1add51360f14e549e95d2f87
vigneshofficial2020 <vigneshofficial2020@gmail.com>
2021-04-25 17:04:27 +0530

    changes

--- tutorial.js
+++ tutorial.js
@@ -1,1 +1,11 @@
 var age = 10;
+var details = [34, 34, 34, 56, "salman", true, false];
+var stringDetails = ["salman", "sa", "true"];
+var numberDetails = [23, 355, 2323];
+var booleanDetails = [true, false, true];
+var objs = { firstName: "salman", age: 24 };
+var objs2 = {
+    firstName: "salman",
+    age: 24
+};
+var mixedDetails = [23, "salman", true];
