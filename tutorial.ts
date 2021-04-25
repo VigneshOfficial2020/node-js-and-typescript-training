177ccf9c833fd6d087fabd4f1929cf59b5ab3d38
vigneshofficial2020 <vigneshofficial2020@gmail.com>
2021-04-25 17:07:52 +0530

    function changes

--- tutorial.ts
+++ tutorial.ts
@@ -1,11 +1,3 @@
-let age: number = 10;
-let details: any[] = [34, 34, 34, 56, "salman", true, false];
-let stringDetails: string[] = ["salman", "sa", "true"];
-let numberDetails: number[] = [23, 355, 2323];
-let booleanDetails: boolean[] = [true, false, true];
-let objs: { firstName: string; age: number } = { firstName: "salman", age: 24 };
-let objs2: { firstName: string; age: number } = {
-  firstName: "salman",
-  age: 24,
-};
-let mixedDetails: [number, string, boolean] = [23, "salman", true];
+function add(a: number, b: number): number {
+  return a + b;
+}
