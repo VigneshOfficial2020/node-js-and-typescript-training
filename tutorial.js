44ff02f8bcbf6923b2a11d31348cb4c9009472a6
vigneshofficial2020 <vigneshofficial2020@gmail.com>
2021-04-25 17:16:32 +0530

    custom type changes

--- tutorial.js
+++ tutorial.js
@@ -1,3 +1,12 @@
-function add(a, b) {
-    return a + b;
-}
+var complex = {
+    age: [34, 34, 23, 123],
+    getAgeDetails: function (all) {
+        return this.age;
+    }
+};
+var complex2 = {
+    age: [34, 343434, 12312312321],
+    getAgeDetails: function (all) {
+        return this.age;
+    }
+};
--- tutorial.ts
+++ tutorial.ts
@@ -1,3 +1,18 @@
-function add(a: number, b: number): number {
-  return a + b;
-}
+type ComplexDetails = {
+  age: number[];
+  getAgeDetails: (all: boolean) => number[];
+};
+
+let complex: ComplexDetails = {
+  age: [34, 34, 23, 123],
+  getAgeDetails: function (all: boolean): number[] {
+    return this.age;
+  },
+};
+
+let complex2: ComplexDetails = {
+  age: [34, 343434, 12312312321],
+  getAgeDetails: function (all: boolean): number[] {
+    return this.age;
+  },
+};
