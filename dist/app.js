10006306f6c16fb4038cdd2d76acaf3d403b081f
Steven Vicky <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-05-02 08:20:44 +0530

    reposnse as json

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,10 @@
 "use strict";
-const express = require("express");
-const app = express();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
 app.listen(4000, () => {
     console.log("server is running in 4000 ");
 });
@@ -10,5 +14,9 @@ app.use((req, res, next) => {
 });
 app.use((req, res, next) => {
     console.log("This is second default execution");
-    res.send("<h1>hello world second</h1>");
+    // res.send("<h1>hello world second</h1>");
+    res.status(200).json({
+        name: "vicky",
+        age: "26",
+    });
 });
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,21 @@
-const express = require("express");
-
+import express, { Request, Response, NextFunction } from "express";
+import todoRoutes from "./routes/todos";
 const app = express();
 
 app.listen(4000, () => {
   console.log("server is running in 4000 ");
 });
 
-app.use((req: any, res: any, next: any) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   console.log("This is first default execution");
   next();
 });
 
-app.use((req: any, res: any, next: any) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
   console.log("This is second default execution");
-  res.send("<h1>hello world second</h1>");
+  // res.send("<h1>hello world second</h1>");
+  res.status(200).json({
+    name: "vicky",
+    age: "26",
+  });
 });
