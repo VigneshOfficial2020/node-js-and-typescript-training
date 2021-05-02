be1ad7e8923cea5c9fdaac4262480620c99e5ef9
Steven Vicky <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-05-02 07:54:22 +0530

    express request and response

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,14 @@
 "use strict";
-const express = require('express');
+const express = require("express");
 const app = express();
 app.listen(4000, () => {
-    console.log('server is running in 4000 ');
+    console.log("server is running in 4000 ");
 });
 app.use((req, res, next) => {
     console.log("This is first default execution");
-    res.send("<h1>hello world</h1>");
+    next();
+});
+app.use((req, res, next) => {
+    console.log("This is second default execution");
+    res.send("<h1>hello world second</h1>");
 });
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,17 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
 app.listen(4000, () => {
-  console.log('server is running in 4000 ');
-  
-})
+  console.log("server is running in 4000 ");
+});
 
 app.use((req: any, res: any, next: any) => {
   console.log("This is first default execution");
-  res.send("<h1>hello world</h1>");
+  next();
+});
+
+app.use((req: any, res: any, next: any) => {
+  console.log("This is second default execution");
+  res.send("<h1>hello world second</h1>");
 });
