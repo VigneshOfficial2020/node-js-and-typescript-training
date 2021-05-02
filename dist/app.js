c06716f9e2f4dfdc63b29cf5d50d5abcee6d3e2f
Steven Vicky <70185865+VigneshOfficial2020@users.noreply.github.com>
2021-05-02 07:27:49 +0530

    first express

--- dist/app.js
+++ dist/app.js
@@ -4,3 +4,7 @@ const app = express();
 app.listen(4000, () => {
     console.log('server is running in 4000 ');
 });
+app.use((req, res, next) => {
+    console.log("This is first default execution");
+    res.send("<h1>hello world</h1>");
+});
--- src/app.ts
+++ src/app.ts
@@ -6,3 +6,8 @@ app.listen(4000, () => {
   console.log('server is running in 4000 ');
   
 })
+
+app.use((req: any, res: any, next: any) => {
+  console.log("This is first default execution");
+  res.send("<h1>hello world</h1>");
+});
